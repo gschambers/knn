347b065d29067794feb818e344a96c8332318f15
Gary Chambers <gary@lipsmack.co.uk>
2012-12-03 13:27:22 +0000

    Fixed line endings

--- src/model.js
+++ src/model.js
@@ -1,7 +1,12 @@
 kNN.Model = (function( global, undefined )
 {
+	"use strict";
+
 	var Model,
-		extractFeatures;
+
+		extractFeatures,
+		predictCategory,
+		sortPoints;
 
 	/**
 	 * Extract feature definitions from
@@ -59,9 +64,83 @@ kNN.Model = (function( global, undefined )
 	};
 
 	/**
+	 * Arrange a list of points by
+	 * ascending distance.
+	 *
+	 * @param {Array} points A multi-dimensional
+	 *     array containing [ point<Point>, distance<Number> ]
+	 * @return {Array} Sorted array of points
+	 */
+	sortPoints = function sortPoints( points )
+	{
+		return points.sort(function(a, b)
+		{
+			return a[1] < b[1] ? -1 : 1;
+		});
+	};
+
+	/**
+	 * Predict the category for a point
+	 * based on its K-nearest neighbors
+	 * @param  {Number} K How many points to compare
+	 * @param  {Point} point
+	 * @param  {Array} neighbors An array of points
+	 * @return {String}
+	 */
+	predictCategory = function predictCategory( K, point, neighbors )
+	{
+		var categories,
+
+			prediction,
+			bestScore,
+
+			force,
+			key,
+			i, n;
+
+		categories = {};
+		i = 0;
+
+		for( ; i < K; i++ )
+		{
+			n = neighbors[i];
+
+			if( !categories.hasOwnProperty(n.category) )
+			{
+				categories[ n.category ] = 0;
+			}
+
+			categories[ n.category ] += 1;
+		}
+
+		for( key in categories )
+		{
+			// If two categories score equally,
+			// choose randomly between the two
+
+			if( categories[key] === bestScore )
+			{
+				if( 1 & Math.random() * 100 >> 0 )
+				{
+					force = true;
+				}
+			}
+
+			if( force || categories[key] > bestScore )
+			{
+				prediction = key;
+				bestScore = categories[key];
+				force = false;
+			}
+		}
+
+		return prediction;
+	};
+
+	/**
 	 * Classification model implementing
 	 * kNN on arbitrary training data.
-	 * 
+	 *
 	 * @param {Object} data Training data
 	 * @constructor
 	 */
@@ -86,22 +165,22 @@ kNN.Model = (function( global, undefined )
 	/**
 	 * Classifies a point based on the
 	 * dimensions of the training data.
-	 * 
+	 *
+	 * @param {Number} K How many adjacent points to compare
 	 * @param {Point} point
 	 * @return {String}
 	 */
-	Model.prototype.classify = function classify( point )
+	Model.prototype.classify = function classify( K, point )
 	{
 		var distance,
-			last,
-			nearest,
+			points,
+
 			f, i, j, p;
 
 		i = this.points.length;
 		j = this.features.length;
 
-		last = Infinity;
-		nearest = null;
+		points = [];
 
 		while( i-- )
 		{
@@ -111,105 +190,20 @@ kNN.Model = (function( global, undefined )
 			for( j in this.features )
 			{
 				f = this.features[ j ];
-				distance += Math.pow(f.calcDistance( point, p ), 2);
-			}
 
-			distance = Math.sqrt( distance );
-
-			if( distance < last )
-			{
-				last = distance;
-				nearest = p;
-			}
-		}
-
-		return nearest.category;
-	};
-
-	return Model;
-	/**
-	 * Arrange a list of points by
-	 * ascending distance.
-	 *
-	 * @param {Array} points A multi-dimensional
-	 *     array containing [ point<Point>, distance<Number> ]
-	 * @return {Array} Sorted array of points
-	 */
-	sortPoints = function sortPoints( points )
-		return points.sort(function(a, b)
-		{
-			return a[1] < b[1] ? -1 : 1;
-	};
-	/**
-	 * Predict the category for a point
-	 * based on its K-nearest neighbors
-	 * @param  {Number} K How many points to compare
-	 * @param  {Point} point
-	 * @param  {Array} neighbors An array of points
-	 * @return {String}
-	 */
-	{
-
-			bestScore,
-			force,
-			key,
-			i, n;
-
-		categories = {};
-		i = 0;
-
-		for( ; i < K; i++ )
-		{
-			n = neighbors[i];
-			if( !categories.hasOwnProperty(n.category) )
-				categories[ n.category ] = 0;
-
-
-		{
-			// choose randomly between the two
-
-			{
-				{
-					force = true;
-				}
-			}
-
-			if( force || categories[key] > bestScore )
-			{
-				bestScore = categories[key];
-			}
-		}
-
-		return prediction;
-	};
-	/**
-	 * Classifies a point based on the
-	 * dimensions of the training data.
-	 * @param {Number} K How many adjacent points to compare
-	 * @param {Point} point
-	 */
-	{
-		var distance,
-			f, i, j, p;
-
-		i = this.points.length;
-
-		points = [];
-
-		while( i-- )
-			p = this.points[ i ];
-			distance = 0;
-
-			{
-				f = this.features[ j ];
 				distance += Math.pow(
 					f.calcDistance( point, p ),
 					2
 				);
+			}
 
 			distance = Math.sqrt( distance );
+			points.push( [p, distance] );
 		}
+
 		points = sortPoints( points );
 		return predictCategory( K, point, points );
 	};
+
+	return Model;
 })( this );
